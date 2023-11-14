36e022977766d78d3b577d6ed812df158ef10658
Leon Jacobs <leonja511@gmail.com>
2023-11-14 16:34:00 +0200

    (feat) add a small packaging helper

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# a small packaging helper
+
+function f_usage() {
+    echo
+    echo "Usage: $0 <subcommand>"
+    echo "Subcommands:"
+    echo "  build       build the latest distribution"
+    echo "  test-upload upload a build to test pypi"
+    echo "  upload      upload a build to pypi"
+}
+
+# parse commandline arguments
+subcommand=$1
+case $subcommand in
+    "" | "usage" | "-h" | "--help")
+        f_usage
+        ;;
+    "build")
+        rm -Rf build/
+        python -m build
+        ;;
+    "test-upload")
+        python -m twine upload --repository testpypi dist/*
+        ;;
+    "upload")
+        python -m twine upload --repository pypi dist/*
+        ;;
+    *)
+        echo "unknown command. check usages."
+        ;;
+esac
+
